3754f0634b164a8d34b3fabcf8030d68131ebffe
Joseph Livengood <joeylivengood@gmail.com>
2016-10-07 17:44:07 +0000

    Inpli bootstrap, added Index/poll creation client sided

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,25 +9,31 @@ module.exports = function (app) {
 
 	app.route('/')
 		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
+			//res.sendFile(path + '/public/index.html');
+			res.render(path + '/public/index')
+		});
+		
+	app.route('/newpoll')
+		.get(function (req, res) {
+			res.render(path + '/public/newpoll')
 		});
 	
 	app.route('/poll/:pollid')
 	    .get(function (req, res) {
 	        mongo.connect(url,function(err,db) {
-	            if (err) console.log(err)
-                var collection=db.collection('polls')
+	            if (err) console.log(err);
+                var collection=db.collection('polls');
                 collection.find({
                     id: req.params.pollid
                 }).toArray(function(err,documents){
-                    if (err) console.log(err)
-                    console.log(documents[0].question)
+                    if (err) console.log(err);
+                    console.log(documents[0].question);
                     res.render(path + '/public/poll', {question: documents[0].question,
-                    options: documents[0].options, asker: documents[0].posterid, date: documents[0].date})
-                    db.close()
-	            })
-	        })
-	    })
+                    options: documents[0].options, asker: documents[0].posterid, date: documents[0].date});
+                    db.close();
+	            });
+	        });
+	    });
 
 	app.route('/login')
 		.get(function (req, res) {
